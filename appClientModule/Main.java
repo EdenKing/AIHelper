db70d517a5d9a15a7c16fefb00e0ea7e5157add5
apple <apple@bogon>
2017-03-24 22:32:39 +0800

    Add constants object

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -1,14 +1,35 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import util.Constants;
 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket svrSocket = new ServerSocket(Constants.port);
+		while (true) {
+			Socket socket = svrSocket.accept();
+			// 足够大的一个缓冲区
+			byte[] buf = new byte[1024 * 1024];
+			InputStream in = socket.getInputStream();
+			int byteRead = in.read(buf, 0, 1024 * 1024);
+			String dataString = new String(buf, 0, byteRead);
+			System.out.println(dataString);
+			in.close();
+			socket.close();
+		}
 	}
 
-	/* (non-Java-doc)
+	/*
+	 * (non-Java-doc)
+	 * 
 	 * @see java.lang.Object#Object()
 	 */
 	public Main() {
-		super();
+
 	}
 
 }
