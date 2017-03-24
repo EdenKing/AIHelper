c9b71159b7c0ac3bcacc775ee5b511856919d3f6
apple <apple@bogon>
2017-03-24 22:32:39 +0800

    Add constants object

---
+++ appClientModule/util/Constants.java
@@ -0,0 +1,26 @@
+package util;
+
+/**
+ * Constants
+ * 
+ * @author Yiyue Wang
+ *
+ */
+public class Constants {
+
+	// Server port
+	public static final int port = 8080;
+
+	/** METHOD_GET value:GET */
+	public static final String METHOD_GET = "GET";
+
+	/** METHOD_POST value:POST */
+	public static final String METHOD_POST = "POST";
+
+	/** METHOD_PUT value:PUT */
+	public static final String METHOD_PUT = "PUT";
+
+	/** METHOD_DELETE value:DELETE */
+	public static final String METHOD_DELETE = "DELETE";
+
+}
